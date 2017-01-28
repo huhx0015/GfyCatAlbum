efad01e8ba72e42f6b453eaefa3277779fc63b60
huhx0015 <huhx0015@gmail.com>
2017-01-28 12:13:02 -0800

    Added constant and RetrofitInterface files.

---
+++ app/src/main/java/com/gfycat/album/constants/GfyConstants.java
@@ -0,0 +1,11 @@
+package com.gfycat.album.constants;
+
+/**
+ * Created by Michael Yoon Huh on 1/28/2017.
+ */
+
+public class GfyConstants {
+
+    // URL:
+    public static final String BASE_URL = "https://api.gfycat.com/v1/";
+}
---
+++ app/src/main/java/com/gfycat/album/interfaces/RetrofitInterface.java
@@ -0,0 +1,13 @@
+package com.gfycat.album.interfaces;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by Michael Yoon Huh on 1/28/2017.
+ */
+
+public interface RetrofitInterface {
+
+}
