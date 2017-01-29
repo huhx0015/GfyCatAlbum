2b25b4ff19a5ce0bf61ea3536f5789d916314f3f
s-tc-chou <steve.tc.chou@gmail.com>
2017-01-29 08:29:16 -0800

    wip

--- app/src/main/java/com/gfycat/album/activities/HandleShareActivity.java
+++ app/src/main/java/com/gfycat/album/activities/HandleShareActivity.java
@@ -1,12 +1,25 @@
 package com.gfycat.album.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+
 import com.gfycat.album.R;
 import com.gfycat.album.application.GfyApplication;
+
+import com.gfycat.album.interfaces.RetrofitInterface;
+import com.gfycat.album.models.CreateGfycatPojo;
+import com.gfycat.album.models.CreateGfycatRequest;
+
+
 import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class HandleShareActivity extends AppCompatActivity {
@@ -47,6 +60,36 @@ public class HandleShareActivity extends AppCompatActivity {
         Log.d("STEVE", sharedText);
 
         // TODO: Display a DialogFragment here. Once new gfycat data has been entered, make a call to create gfycat endpoint; also store reference locally.
-    }
 
+        RetrofitInterface refreshTokenRequest = retrofitAdapter.create(RetrofitInterface.class);
+        Call<CreateGfycatPojo> call = refreshTokenRequest.createGfycat(new CreateGfycatRequest("http://i.imgur.com/QlxKcey.gif", "cat sneezing"));
+        call.enqueue(new Callback<CreateGfycatPojo>() {
+            @Override
+            public void onResponse(Call<CreateGfycatPojo> call, Response<CreateGfycatPojo> response) {
+
+                if (response.isSuccessful()) {
+                    /*GrantResponsePojo responsePojo = response.body();
+
+                    SharedPreferences gfyPrefs = GfyPreferences.initializePreferences(HandleShareActivity.this);
+                    GfyPreferences.setUserLoggedIn(true, gfyPrefs);
+                    GfyPreferences.setAccessToken(responsePojo.getAccessToken(), gfyPrefs);
+                    GfyPreferences.setRefreshToken(responsePojo.getRefreshToken(), gfyPrefs);
+
+                    launchMainIntent();*/
+                    Log.d("onResponse: ", "success");
+
+                    //grab gfycat name
+
+                    //log record in local db as well
+
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CreateGfycatPojo> call, Throwable t) {
+                Log.d("onFailure: ", "oh no");
+            }
+        });
+    }
 }
